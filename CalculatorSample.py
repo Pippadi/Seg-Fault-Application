86057338f2551526b17934497d67bdce15ddcbbc
ppcmdgames <ppcmdgames@gmail.com>
2020-11-16 16:34:12 +0530

    Update CalculatorSample.py

--- CalculatorSample.py
+++ CalculatorSample.py
@@ -8,7 +8,7 @@ class Window:
         program.title=Label(win, text="Sample Calculator", fg="blue", font=("Arial", 15))
         program.title.place(x=110, y=70)
         program.lbl1=Label(win, text="First Number")
-        program.lbl1=Label(win, text="Second Number")
+        program.lbl2=Label(win, text="Second Number")
         program.t1=Entry(bd=2)
         program.t2=Entry(bd=2)
         program.rtxt=Entry(bd=2)
@@ -16,15 +16,21 @@ class Window:
         program.subbtn=Button(win, text="-")
         program.multibtn=Button(win, text="*")
         program.dividebtn=Button(win, text="/")
+        # Specify locations
+        program.lbl1.place(x=, y=)
+        program.lbl2.place(x=, y=)
+        program.t1.place(x=, y=)
+        program.t2.place(x=, y=)
+        program.rtxt.place(x=, y=)
+        
     def add():
-        result=0
+        result=
     def subtract():
         result=1
     def multiply():
         result=5
     def divide():
         result=4
-        
 
 window=Tk()
 mywin=Window(window)
