831ce6f412dbbd05ddc0fb6e4dcb24b06d4b370c
ppcmdgames <ppcmdgames@gmail.com>
2020-11-16 15:47:01 +0530

    Update CalculatorSample.py
    Program is now working

--- CalculatorSample.py
+++ CalculatorSample.py
@@ -4,8 +4,8 @@ from tkinter import *
 
 class Window:
     def __init__(program, win):
-        program.title=Label(win, text="Sample Calculator", fg="blue")
-        program.title.place(x=0, y=100)
+        program.title=Label(win, text="Sample Calculator", fg="blue", font=("Arial", 15))
+        program.title.place(x=110, y=70)
         program.lbl1=Label(win, text="First Number")
         program.lbl1=Label(win, text="Second Number")
     def add():
@@ -20,5 +20,5 @@ class Window:
 window=Tk()
 mywin=Window(window)
 window.title('Calculator Program')
-window.geometry("400x300+10+10")
+window.geometry("400x400+10+10")
 window.mainloop()
