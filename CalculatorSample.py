acc0cf80dee098a230bfc960f02e0d9d922e8bc0
ppcmdgames <ppcmdgames@gmail.com>
2020-11-16 15:37:06 +0530

    Added incomplete functions
    Added functions which aren't working yet.

--- CalculatorSample.py
+++ CalculatorSample.py
@@ -2,8 +2,21 @@
 
 from tkinter import *
 
+class Window:
+    def __init__(program, win):
+        program.title=Label(win, text="Sample Calculator")
+        program.lbl1=()
+    def add():
+        
+    def subtract():
+        
+    def multiply():
+        
+    def divide():
+    
+
 window=Tk()
+mywin=Window(window)
 window.title('Calculator Program')
 window.geometry("400x300+10+10")
 window.mainloop()
-
