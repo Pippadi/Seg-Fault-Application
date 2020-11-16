523e1476058c8ef640d215665d0b82e92d4edba2
ppcmdgames <ppcmdgames@gmail.com>
2020-11-16 15:41:58 +0530

    Update CalculatorSample.py

--- CalculatorSample.py
+++ CalculatorSample.py
@@ -4,16 +4,18 @@ from tkinter import *
 
 class Window:
     def __init__(program, win):
-        program.title=Label(win, text="Sample Calculator")
-        program.lbl1=()
+        program.title=Label(win, text="Sample Calculator", fg="blue")
+        program.title.place(x=0, y=100)
+        program.lbl1=Label(win, text="First Number")
+        program.lbl1=Label(win, text="Second Number")
     def add():
-        
+        result=0
     def subtract():
-        
+        result=1
     def multiply():
-        
+        result=5
     def divide():
-    
+        result=4
 
 window=Tk()
 mywin=Window(window)
