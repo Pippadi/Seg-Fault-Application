2777fad85903dc02fb0d4a776b2c9b41c39b0d73
ppcmdgames <ppcmdgames@gmail.com>
2020-11-16 16:20:10 +0530

    Added more variables

--- CalculatorSample.py
+++ CalculatorSample.py
@@ -3,11 +3,19 @@
 from tkinter import *
 
 class Window:
+    #  initializing function
     def __init__(program, win):
         program.title=Label(win, text="Sample Calculator", fg="blue", font=("Arial", 15))
         program.title.place(x=110, y=70)
         program.lbl1=Label(win, text="First Number")
         program.lbl1=Label(win, text="Second Number")
+        program.t1=Entry(bd=2)
+        program.t2=Entry(bd=2)
+        program.rtxt=Entry(bd=2)
+        program.addbtn=Button(win, text="+")
+        program.subbtn=Button(win, text="-")
+        program.multibtn=Button(win, text="*")
+        program.dividebtn=Button(win, text="/")
     def add():
         result=0
     def subtract():
@@ -16,6 +24,7 @@ class Window:
         result=5
     def divide():
         result=4
+        
 
 window=Tk()
 mywin=Window(window)
