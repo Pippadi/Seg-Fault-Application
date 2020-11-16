f158ef16e1a15b10a478f1d1dc71c2d06d0d543d
ppcmdgames <ppcmdgames@gmail.com>
2020-11-16 15:23:42 +0530

    Create CalculatorSample.py

---
+++ CalculatorSample.py
@@ -0,0 +1,9 @@
+# A sample program that performs basic mathematical operations
+
+from tkinter import *
+
+window=Tk()
+window.title('Calculator Program')
+window.geometry("400x300+10+10")
+window.mainloop()
+
