51e979788ad3a9f4421289009832740d56841c38
ppcmdgames <ppcmdgames@gmail.com>
2020-11-17 19:00:49 +0530

    Update CSVSample.py

--- CSVSample.py
+++ CSVSample.py
@@ -1,6 +1,7 @@
 # A sample program to read data from .CSV files
 
 import pandas as pd
+import time
 
 data = pd.read_csv('population.csv')
 
@@ -15,5 +16,16 @@ firstn = int(input())
 print(data.head(firstn))
 
 # Display the names of all columns
-print("Displaying the names of all ")
+print("\nDisplaying the names of all ")
 print(data.columns)
+
+# Display a specific column
+print("Enter the name of any column whose values to display: ")
+cname = input()
+# Display the values from the top
+print(data[cname])
+
+# Exit the program
+print("\n Exiting in 5 s from now...")
+# Wait for 5 seconds before exiting
+time.sleep(5)
