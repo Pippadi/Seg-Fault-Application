995af13a10da87da1c9405a1340746abbe0c9bb3
ppcmdgames <ppcmdgames@gmail.com>
2020-11-17 18:51:24 +0530

    Update CSVSample.py

--- CSVSample.py
+++ CSVSample.py
@@ -9,8 +9,11 @@ print(data)
 
 # Print only the first few values
 print("Enter the number of initial values to display: ")
+# Parse to integer
 firstn = int(input())
+# Display the values from the top
 print(data.head(firstn))
 
-# Display all columns
-
+# Display the names of all columns
+print("Displaying the names of all ")
+print(data.columns)
