03181b7be99bdaadfa2332ced4ab17f67ec1bafa
ppcmdgames <ppcmdgames@gmail.com>
2020-11-17 18:48:11 +0530

    More code

--- CSVSample.py
+++ CSVSample.py
@@ -4,4 +4,13 @@ import pandas as pd
 
 data = pd.read_csv('population.csv')
 
+# Print all values
 print(data)
+
+# Print only the first few values
+print("Enter the number of initial values to display: ")
+firstn = int(input())
+print(data.head(firstn))
+
+# Display all columns
+
--- main.py
+++ main.py
@@ -2,52 +2,64 @@
 # Currently working code
 
 from tkinter import *
+from tkinter.ttk import Combobox
+import pandas as pd
+
+num1 = 1
+data=pd.read_csv('population.csv')
 
 class Window:
   def __init__(program, win):
     # List of coordinates
-    xc=("1", "2", "3")
+    program.xc=("1", "2", "3")
     # Code to add widgets
-    btn1=Button(window, text="Button 1")
+    btn1=Button(win, text="Button 1", command=program.getx)
     btn1.place(x=150, y=250)
-    btn2=Button(window, text="Button 2")
+    btn2=Button(win, text="Button 2")
     btn2.place(x=300, y=250)
-    title=Label(window, text="Title", fg="black", font=("Arial", 16))
+    title=Label(win, text="Title", fg="black", font=("Arial", 16))
     title.place(x=200, y=35)
-    #txtx=Entry(window, text="Enter the X-coordinate", bd=2)
-    #txtx.place(x=150, y=100)
-    txty=Entry(window, text="Enter the Y-coordinate", bd=2)
+    # program.txtx=Entry(win, text="Enter the X-coordinate", bd=2)
+    # program.txtx.place(x=150, y=100)
+    txty=Entry(win, text="Enter the Y-coordinate", bd=2)
     txty.place(x=150, y=150)
-    lblx=Label(window, text="X-Coordinate")
+    lblx=Label(win, text="X-Coordinate")
     lblx.place(x=20, y=100)
-    lbly=Label(window, text="Y-Coordinate")
+    lbly=Label(win, text="Y-Coordinate")
     lbly.place(x=20, y=150)
-    lb=Listbox(window, height=2)
-    for ncoordinates in xc:
-      lb.insert(END, ncoordinates)
-    lb.place(x=150, y=95)
-    
-  # def avgtime():
+    ddx=Combobox(win, values=program.xc)
+    ddx.place(x=150, y=100)
+    lbx=Listbox(win, height=2)
+    for ncoordinates in program.xc:
+     lbx.insert(END, ncoordinates)
+    lbx.place(x=150, y=200)
+    # Bind to functions
+    btn1.bind('<Button-1>', program.getx)
+  #def avgtime():
     # Get the average time
-
-  # def getcoordinates():
+    
+  #def getcoordinates(xc, yc):
     # 
-  
-  # def filterd():
+    
+  #def filterd():
     # 
-  
+    
   #def filterr():
     # 
-
+    
   #def filterbp():
     # 
-
+    
   #def filterage(above60):
     #
-  
+    
     #if (above60==True):
     
     #else:
+  
+  def getx(program, value):
+    # For debug only
+    print(data.head(5))
     
 
 window=Tk()
@@ -55,4 +67,3 @@ programwin=Window(window)
 window.title('Application')
 window.geometry("500x500+10+10")
 window.mainloop()
-
