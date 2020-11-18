61a919f0b3b0e863932c41d42d03127e3e465be8
ppcmdgames <ppcmdgames@gmail.com>
2020-11-18 12:39:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,39 +17,48 @@ reptime=0
 class Window:
   def __init__(program, win):
     # List of coordinates
-    coordinates=("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20")
+    program.coordinates=("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20")
     # Code to add widgets
-    btn1=Button(win, text="Find", command=program.getcoordinates)
-    btn1.place(x=150, y=250)
-    btn2=Button(win, text="Time", command=program.avgtime)
-    btn2.place(x=300, y=250)
-    title=Label(win, text="Title", fg="black", font=("Arial", 16))
-    title.place(x=200, y=35)
-    # txtx=Entry(win, text="Enter the X-coordinate", bd=2)
-    # txtx.place(x=150, y=100)
-    # txty=Entry(win, text="Enter the Y-coordinate", bd=2)
-    # txty.place(x=150, y=150)
-    lblx=Label(win, text="X-Coordinate")
-    lblx.place(x=20, y=100)
-    lbly=Label(win, text="Y-Coordinate")
-    lbly.place(x=20, y=150)
-    ddx=Combobox(win, values=coordinates)
-    ddx.place(x=150, y=100)
-    ddy=Combobox(win, values=coordinates)
-    ddy.place(x=150, y=150)
+    program.btn1=Button(win, text="Find", command=program.getcoordinates)
+    program.btn1.place(x=150, y=250)
+    program.btn2=Button(win, text="Time", command=program.avgtime)
+    program.btn2.place(x=300, y=250)
+    program.title=Label(win, text="Title", fg="black", font=("Arial", 16))
+    program.title.place(x=200, y=35)
+    # program.txtx=Entry(win, text="Enter the X-coordinate", bd=2)
+    # program.txtx.place(x=150, y=100)
+    # program.txty=Entry(win, text="Enter the Y-coordinate", bd=2)
+    # program.txty.place(x=150, y=150)
+    program.lblx=Label(win, text="X-Coordinate")
+    program.lblx.place(x=20, y=100)
+    program.lbly=Label(win, text="Y-Coordinate")
+    program.lbly.place(x=20, y=150)
+    program.ddx=Combobox(win, values=program.coordinates)
+    program.ddx.place(x=150, y=100)
+    program.ddy=Combobox(win, values=program.coordinates)
+    program.ddy.place(x=150, y=150)
     # Bind to functions
-    btn1.bind('<Button-1>', program.getcoordinates)
-    btn2.bind('<Button-1>', program.avgtime)
+    program.btn1.bind('<Button-1>', program.getcoordinates)
+    program.btn2.bind('<Button-1>', program.avgtime)
 
-  def avgtime(num1):
-    print(dataset.head[2:3])
+  def avgtime(program):
+    # print(dataset.head[2:3])
     # Get the average time
     
+    # For Debug only
+    ycoordinate=int(program.ddy.get())
+    rown=ycoordinate-1
+    colmn=1
+    tmr=int(dataset.iloc[rown,1])
+    tmi=int(dataset.iloc[rown,0])+1
+    print("\nThe total time taken is: ")
+    print(tmr-tmi)
 
   def getcoordinates(program):
     # For debug only
-    print(pp.head(5))
-    value=
+    # print(pp.head(5))
+    value=program.ddx.get()
+    print("\n")
     print(value)
     
   #def getcoordinates(xc, yc):
